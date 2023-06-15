df8edef728bf30e061d614b42607a2424f5a315b
Nahuel Cerdan <nahuel.cerdan.12@gmail.com>
2023-06-14 23:24:32 -0300

    Base del Proyecto Final

---
+++ project/apps/producto/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request: HttpResponse) -> HttpResponse:
+    return render(request, "producto/index.html")
