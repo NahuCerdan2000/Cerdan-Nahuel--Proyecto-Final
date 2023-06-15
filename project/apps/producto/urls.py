03cb7531e7f62f6fa859a052d7c30d286b96e46f
Nahuel Cerdan <nahuel.cerdan.12@gmail.com>
2023-06-15 15:51:02 -0300

    Plantilla de Categoria de Productos Terminada

--- project/apps/producto/urls.py
+++ project/apps/producto/urls.py
@@ -4,6 +4,16 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("producto_categoria_list", views.producto_categoria_list, name="producto_categoria_list"),
-    path("producto_categoria_create", views.producto_categoria_create, name="producto_categoria_create"),
+    path("producto_categoria_list/", views.ProductoCategoriaList.as_view(), name="producto_categoria_list"),
+    path("producto_categoria_create/", views.ProductoCategoriaCreate.as_view(), name="producto_categoria_create"),
+    path(
+        "producto_categoria_delete/<int:pk>/",
+        views.ProductoCategoriaDelete.as_view(),
+        name="producto_categoria_delete",
+    ),
+    path(
+        "producto_categoria_update/<int:pk>/",
+        views.ProductoCategoriaUpdate.as_view(),
+        name="producto_categoria_update",
+    ),
 ]
--- project/apps/producto/views.py
+++ project/apps/producto/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.template import context
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from . import forms, models
 
@@ -10,20 +13,67 @@ def index(request: HttpResponse) -> HttpResponse:
     return render(request, "producto/index.html")
 
 
-def producto_categoria_list(request):
-    categorias = models.ProductoCategoria.objects.all()
-    context = {
-        "categorias": categorias,
-    }
-    return render(request, "producto/producto_categoria_list.html", context)
+class ProductoCategoriaList(ListView):
+    model = models.ProductoCategoria
+    template_name = "producto/producto_categoria_list.html"
+    context_object_name = "categorias"
 
 
-def producto_categoria_create(request):
-    if request.method == "POST":
-        form = forms.ProductoCategoriaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("producto:index")
-    else:
-        form = forms.ProductoCategoriaForm()
-    return render(request, "producto/producto_categoria_create.html", {"form": form})
+class ProductoCategoriaCreate(CreateView):
+    model = models.ProductoCategoria
+    form_class = forms.ProductoCategoriaForm
+    template_name = "producto/producto_categoria_create.html"
+    success_url = reverse_lazy("producto:index")
+
+
+class ProductoCategoriaDelete(DeleteView):
+    model = models.ProductoCategoria
+    template_name = "producto/producto_categoria_delete.html"
+    success_url = reverse_lazy("producto:index")
+
+
+class ProductoCategoriaUpdate(UpdateView):
+    model = models.ProductoCategoria
+    form_class = forms.ProductoCategoriaForm
+    template_name = "producto/producto_categoria_update.html"
+    success_url = reverse_lazy("producto:index")
+
+
+#
+# def producto_categoria_list(request):
+#    categorias = models.ProductoCategoria.objects.all()
+#    context = {
+#        "categorias": categorias,
+#    }
+#    return render(request, "producto/producto_categoria_list.html", context)
+#
+#
+# def producto_categoria_create(request):
+#    if request.method == "POST":
+#        form = forms.ProductoCategoriaForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+#            return redirect("producto:index")
+#    else:
+#        form = forms.ProductoCategoriaForm()
+#    return render(request, "producto/producto_categoria_create.html", {"form": form})
+#
+#
+# def producto_categoria_delete(request, id):
+#    categoria = models.ProductoCategoria.objects.get(id=id)
+#    if request.method == "POST":
+#        categoria.delete()
+#        return redirect("producto:index")
+#    return render(request, "producto/producto_categoria_delete.html", {"categoria": categoria})
+#
+#
+# def producto_categoria_update(request, id):
+#    categoria = models.ProductoCategoria.objects.get(id=id)
+#    if request.method == "POST":
+#        form = forms.ProductoCategoriaForm(request.POST, instance=categoria)
+#        if form.is_valid():
+#            form.save()
+#            return redirect("producto:index")
+#    else:
+#        form = forms.ProductoCategoriaForm(instance=categoria)
+#    return render(request, "producto/producto_categoria_update.html", {"form": form})
