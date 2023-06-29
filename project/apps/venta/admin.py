9330c80f4cb4c59ba7e4d35793cf8b15e48caaec
Nahuel Cerdan <nahuel.cerdan.12@gmail.com>
2023-06-29 02:29:12 -0300

    Ventas desde panel

--- project/apps/venta/admin.py
+++ project/apps/venta/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+
 from . import models
 
 # Register your models here.
 
 admin.site.register(models.Venderdor)
+admin.site.register(models.Venta)
--- project/apps/venta/models.py
+++ project/apps/venta/models.py
@@ -1,5 +1,9 @@
+from time import timezone
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
 from personal.models import Personal
 
 # Create your models here.
@@ -36,3 +40,19 @@ class Meta:
 
 def __str__(self):
     return f"{self.user.username}"
+
+
+class Venta(models.Model):
+    vendedor = models.ForeignKey(Venderdor, on_delete=models.DO_NOTHING)
+    producto = models.ForeignKey("producto.Producto", on_delete=models.DO_NOTHING)
+    cantidad = models.PositiveBigIntegerField()
+    precio_total = models.DecimalField(decimal_places=2, max_digits=10, editable=False)
+    fecha = models.DateTimeField(default=timezone.now, editable=False)
+
+    def clean(self):
+        if self.cantidad > self.producto.cantidad:
+            raise ValidationError("No hay suficiente stock para ejecutar la venta")
+
+    def save(self, *args, **kwargs):
+        self.precio_total = self.cantidad * self.producto.precio
+        super().save(*args, **kwargs)
